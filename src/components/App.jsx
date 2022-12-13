09127fb515cbb07e4441bd8762ccd07a7b77f76d
ilyalazarenkoit <ilyalazarenkoit@gmail.com>
2022-12-13 23:22:15 +0200

    urrrraaaa

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,39 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { ImageGallery } from './ImageGallery/ImageGallery';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { Modal } from './Modal/Modal';
+import './styles.css';
+
+class App extends Component {
+  state = {
+    showModal: false,
+    imageURL: '',
+  };
+
+  toggleModal = imageURL => {
+    this.setState(({ showModal }) => ({
+      showModal: !showModal,
+    }));
+    this.setState({ imageURL: imageURL });
+  };
+  closeModal = () => {
+    this.setState({ showModal: false });
+  };
+
+  render() {
+    return (
+      <>
+        <ImageGallery toggleModal={this.toggleModal}></ImageGallery>
+        {this.state.showModal && (
+          <Modal closeModal={this.closeModal}>
+            <img width="700" height="500" src={this.state.imageURL} alt="" />
+          </Modal>
+        )}
+        <ToastContainer autoClose={2000} />
+      </>
+    );
+  }
+}
+
+export { App };
